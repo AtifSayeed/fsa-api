7649d11ba06f3070ca1fb41aaec09acce9d5a0c0
Mohammed Atif Sayeed <mdatifsayeed@gmail.com>
2023-01-05 22:52:30 +0530

    Login Authenticated..

--- fsa-react/src/App.js
+++ fsa-react/src/App.js
@@ -12,7 +12,6 @@ function App(){
    return  <div>
 <BrowserRouter>
  <Header/>
- <Login/>
 <Routes>
    <Route path="/" element={<Home/>}/>
    <Route path="/contact" element={<Contact/>}/>
--- fsa-react/src/Login.js
+++ fsa-react/src/Login.js
@@ -1,28 +1,87 @@
-import React from 'react'
+import axios from "./utils/axios";
+import React, { useState } from "react";
+import ShouldRender from "./common/ShouldRender";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [user, setuser] = useState({
+    email: "",
+    password: "",
+  });
+  const [hasError, setError] = useState(false);
 
-return <div  className='container'>
-    <h1>Login</h1>
-    <div  className='col-3'>
-        <form>
-            <div>
-                <label for='email' className='form-label'>Email</label>
-                <input id='email' className='form-control' type="text" placeholder="Email" autoComplete='off'/>
-            </div>
-            <div>
-                <label for='password' className='form-label'>Password</label>
-                <input id='password' className='form-control' type="password" placeholder="Password" autoComplete='off' />
-            </div>
-            <div className='center m-3'>
-                <button className='btn btn-sm btn-info' type='button'>Login</button>
-            </div>
-            <hr/>
-            <div className='m-3'>
-                <button className='btn btn-sm btn-warning' type='button'>Register</button>
-            </div>
+  const navigate = useNavigate();
+
+  const onLogin = async (evt) => {
+    try {
+      console.log("Logging in...");
+      evt.preventDefault();
+      console.log(user);
+      const res = await axios().post("/api/users/signin", user);
+      localStorage.setItem("user", JSON.stringify(res.data));
+      //navigating...
+      navigate("/");
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  const onInputChange = (evt) => {
+    const newUser = { ...user, [evt.target.name]: evt.target.value };
+    setuser(newUser);
+  };
+
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <div className="col-5">
+        <ShouldRender cond={hasError}>
+          <div className="alert alert-danger">Wrong username or password</div>
+        </ShouldRender>
+
+        <form onSubmit={onLogin}>
+          <div>
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              onChange={onInputChange}
+              name="email"
+              id="email"
+              className="form-control"
+              type="text"
+              placeholder="Email"
+              autoComplete="off"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              onChange={onInputChange}
+              name="password"
+              id="password"
+              className="form-control"
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+            />
+          </div>
+          <div className="center m-3">
+            <button className="btn btn-sm btn-info" type="submit">
+              Login
+            </button>
+          </div>
+          <hr />
+          <div className="m-3">
+            <button className="btn btn-sm btn-warning" type="button">
+              Register
+            </button>
+          </div>
         </form>
+      </div>
     </div>
-</div>
-}
+  );
+};
 export default Login;
